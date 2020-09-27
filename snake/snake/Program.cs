37fbe78efee365993f653954fe30a318742b6701
Anastasia <bylkova-anastasi@mail.ru>
2020-09-27 22:41:47 +0300

    Змейка реагирует на нажатия клавиш

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
 
 namespace snake
 {
@@ -14,15 +17,27 @@ namespace snake
             HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
             VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
             VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            upline.Drow();
-            downline.Drow();
-            leftLine.Drow();
-            rightLine.Drow();
+            upline.Draw();
+            downline.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
 
             // Отрисовка точек
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Drow();
+            snake.Draw();
+
+            while(true)
+            {
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
+
         }
     }
 }
--- snake/snake/Snake.cs
+++ snake/snake/Snake.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace snake
 {
     class Snake : Figure
     {
-        public Snake(Point tail, int lenght, Direction direction)
+        public Direction direction;
+
+        public Snake(Point tail, int lenght, Direction _direction)
         {
+            direction = _direction;
             pList = new List<Point>();
             for(int i = 0; i < lenght; i++)
             {
@@ -15,6 +19,37 @@ namespace snake
                 p.Move(i, direction);
                 pList.Add(p);
             }
+        }
+
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
+        }
+
+        public void HandleKey(ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow)
+                direction = Direction.LEFT;
+            else if (key == ConsoleKey.RightArrow)
+                direction = Direction.RIGHT;
+            else if (key == ConsoleKey.DownArrow)
+                direction = Direction.DOWN;
+            else if (key == ConsoleKey.UpArrow)
+                direction = Direction.UP;
         }
     }
 }
