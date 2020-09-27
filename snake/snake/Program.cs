2b1ea161211f463c6d18f5f246744ff7c131d776
Anastasia <bylkova-anastasi@mail.ru>
2020-09-28 00:22:54 +0300

    Обрабатываем столкновения

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -9,48 +9,13 @@ namespace snake
     class Program
     {
         static void Main(string[] args)
-        {
-            VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
-            Draw(v1);
+        {            
+            Console.SetBufferSize(80, 25);
 
-            Point p = new Point(4, 5, '*');
-            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
-            Draw(fSnake);
-            Snake snake = (Snake)fSnake;
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
-            HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
-
-            List<Figure> figures = new List<Figure>();
-            figures.Add(fSnake);
-            figures.Add(v1);
-            figures.Add(h1);
-
-            foreach (var f in figures)
-            {
-                f.Draw();
-            }
-        }
-
-        static void Draw(Figure figure)
-        {
-            figure.Draw();
-        }
-    }
-}
-
-            /*Console.SetBufferSize(80, 25);
-
-            //Отрисовка рамочки
-            HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            upline.Draw();
-            downline.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
-
-            // Отрисовка точек
+           // Отрисовка точек
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
@@ -61,6 +26,11 @@ namespace snake
 
             while(true)
             {
+                if(walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+
                 if(snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
@@ -82,4 +52,4 @@ namespace snake
 
         }
     }
-}*/
+}
---
+++ snake/snake/Walls.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake
+{
+    class Walls
+    {
+        List<Figure> wallList;
+
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallList = new List<Figure>();
+
+           //Отрисовка рамочки
+           HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
+           HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
+           VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+           VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+
+           wallList.Add(upline);
+           wallList.Add(downline);
+           wallList.Add(leftLine);
+           wallList.Add(rightLine);
+        }
+
+        internal bool IsHit(Figure figure)
+        {
+            foreach (var wall in wallList)
+            {
+                if (wall.IsHit(figure))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Draw()
+        {
+            foreach(var wall in wallList)
+            {
+                wall.Draw();
+            }
+        }
+    }
+}
